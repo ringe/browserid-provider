7c55cf2592c0056516b10f9ce2201dc4de1c49b4
ringe <runar@rin.no>
2012-04-18 07:30:36 +0200

    add default paths

--- lib/browserid-provider/provider.rb
+++ lib/browserid-provider/provider.rb
@@ -2,11 +2,22 @@ module BrowserID
   module Provider
 
     # The BrowserID Provider Rack App
+  #match ".well-known/:action" => 'verified_emails'
+  #get "provision(/:email(/:duration))" => 'verified_emails#provision'
+  #get "whoami" => 'verified_emails#whoami'
+  #post "certify" => 'verified_emails#certify'
+    # Default paths are:
+    #  GET  /.well-known/browserid
+    #  GET  /whoami
+    #  GET  /provision
+    #  POST /certify
     class App
       def initialize(app, options = {})
+        @urls = ["/.well-known/browserid", "/whoami", "/provision", "/certify"]
+
         @app = app
-        @urls = options[:urls]
-        @root = options[:root]
+#        @urls = options[:urls]
+#        @root = options[:root]
       end
 
       # Rack enabled!
